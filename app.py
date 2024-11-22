0a3c97e794bc8819d8c33c69b8ad677c0b7c21f1
wangzi <wangzi@wangzideMacBook-Air.local>
2024-11-22 20:11:17 +0800

    Update app.py to bind to 0.0.0.0

--- app.py
+++ app.py
@@ -57,5 +57,6 @@ def web_predict():
         return render_template('error.html', error=str(e))
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    # 从环境变量中获取端口，默认为 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
